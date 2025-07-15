29cb0db7b7896ea86a1eb33c7dcb471e5b9dd004
Massimo Bonanni <massimo.bonanni@tiscali.it>
2025-07-15 15:51:13 +0000

    Add SmellCode class with examples of security vulnerabilities

---
+++ CodeQLDemo.Console/SmellCode.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Security.Cryptography;
+using System.Data.SqlClient;
+
+public class SmellCode
+{
+    // Expose Sensitive Data
+    public void ExposeSensitiveData()
+    {
+        string password = Console.ReadLine();
+        Console.WriteLine("Your password is: " + password);
+    }
+
+    // Weak Cryptography
+    public void EncryptData()
+    {
+        var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
+        des.GenerateKey();
+        des.GenerateIV();
+    }
+
+    // SQL Injection Risk
+    public void SQLInjection(string username)
+    {
+        var query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+        var command = new SqlCommand(query, new SqlConnection("..."));
+        command.ExecuteReader();
+    }
+
+    // Credential hard coded
+    public void CredentialsHardcoded()
+    {
+        string apiKey = "12345-ABCDE";
+    }
+}
