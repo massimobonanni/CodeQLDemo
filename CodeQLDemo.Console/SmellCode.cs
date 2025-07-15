1014c086bb5443022b756d22e2b2d6928285b741
Massimo Bonanni <massimo.bonanni@tiscali.it>
2025-07-15 15:58:33 +0000

    Remove sensitive data exposure examples from SmellCode class

--- CodeQLDemo.Console/SmellCode.cs
+++ CodeQLDemo.Console/SmellCode.cs
@@ -7,29 +7,29 @@ public class SmellCode
     // Expose Sensitive Data
     public void ExposeSensitiveData()
     {
-        string password = Console.ReadLine();
-        Console.WriteLine("Your password is: " + password);
+        //string password = Console.ReadLine();
+        //Console.WriteLine("Your password is: " + password);
     }
 
     // Weak Cryptography
     public void EncryptData()
     {
-        var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
-        des.GenerateKey();
-        des.GenerateIV();
+        //var des = new DESCryptoServiceProvider(); // 🔥 Weak algorithm
+        //des.GenerateKey();
+        //des.GenerateIV();
     }
 
     // SQL Injection Risk
     public void SQLInjection(string username)
     {
-        var query = "SELECT * FROM Users WHERE Username = '" + username + "'";
-        var command = new SqlCommand(query, new SqlConnection("..."));
-        command.ExecuteReader();
+        //var query = "SELECT * FROM Users WHERE Username = '" + username + "'";
+        //var command = new SqlCommand(query, new SqlConnection("..."));
+        //command.ExecuteReader();
     }
 
     // Credential hard coded
     public void CredentialsHardcoded()
     {
-        string apiKey = "12345-ABCDE";
+        //string apiKey = "12345-ABCDE";
     }
 }
